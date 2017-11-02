25f2548817799b1d0faf6d6f3a554f075fb7bce5
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-11-01 19:50:58 -0500

    Added skin_mask
    Function to detect skin pixels.

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 16:35:33 2017
+
+@author: ron
+"""
+
---
+++ src/preprocess.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 16:56:10 2017
+
+@author: ron
+"""
+import numpy as np
+import cv2
+
+def skin_mask(image,kernel_size=3,erode_iter=1,dilate_iter=1,debug=False):
+    """
+    Performs foreground extraction, typically a hand using simple thresholds in
+    YCrCb color space followed by morphological Opening (dilation of erosion).
+    [INPUT]:
+        image (numpy.ndarray)- an RGB image 
+        kernel_size (int)- Size of kernel used in filtering and morphology. Should be odd.
+        erode_iter (int)- Number of erosion iterations to be performed.
+        dilate_iter (int)- Number of dilation iterations to be performed.
+        debug (bool)- If set to true then result of each operation is saved in the data folder
+                    with filename starting with 'skin_mask'
+    [OUTPUT]:
+        A binary mask of type np.ndarray
+    """
+    assert isinstance(image,np.ndarray), "Input not a numpy array"
+    assert image.ndim == 3, "Expected 3 channels, got %d" %image.ndim
+    assert isinstance(kernel_size,int) and isinstance(erode_iter,int) and isinstance(dilate_iter,int),"Optional argument not an integer"
+    assert kernel_size%2!=0, "Kernel size not odd"
+    kernel = np.ones((kernel_size,kernel_size),dtype=np.uint8)
+    
+    image_ycbcr = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
+    lower = np.array([90,105,130],dtype=np.uint8)
+    upper = np.array([230,135,170],dtype=np.uint8)
+    mask = cv2.inRange(image_ycbcr,lower,upper)
+    mask = cv2.medianBlur(mask,kernel_size)
+    output = cv2.erode(mask,kernel,iterations=erode_iter)
+    output = cv2.dilate(output,kernel,iterations=dilate_iter)
+    
+    
+    if debug:
+        cv2.imwrite("data/skin_mask_input.jpg",image)
+        cv2.imwrite("data/skin_mask_input_cvt.jpg",image_ycbcr)    
+        cv2.imwrite("data/skin_mask_mask.jpg",mask)
+        cv2.imwrite("data/skin_mask_output.jpg",output)
+    return output
+    
